0ceb9602c27b2c9a8192924c3eb829b04c091301
admin <bagheri.saleh@gmail.com>
2018-06-06 23:58:02 +0430

    change product map

--- Libraries/Nop.Data/Mapping/Catalog/ProductMap.cs
+++ Libraries/Nop.Data/Mapping/Catalog/ProductMap.cs
@@ -15,6 +15,7 @@ namespace Nop.Data.Mapping.Catalog
             this.ToTable("Product");
             this.HasKey(p => p.Id);
             this.Property(p => p.Name).IsRequired().HasMaxLength(400);
+            this.Property(p => p.Author).HasMaxLength(400);
             this.Property(p => p.MetaKeywords).HasMaxLength(400);
             this.Property(p => p.MetaTitle).HasMaxLength(400);
             this.Property(p => p.Sku).HasMaxLength(400);
